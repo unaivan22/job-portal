9801ff0c6e25a99d2d81434dcfcb4b784d92829e
unaivan <unaivan@gmail.com>
2022-10-14 08:23:24 +0700

    commit

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './misc/Header'
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+       <div className="container mx-auto sm:px-24 md:px-32 xl:px-32 lg:px-32">
+        
+            <section>
+                
+            </section>
+
+       </div>
+    </div>
+  )
+}
